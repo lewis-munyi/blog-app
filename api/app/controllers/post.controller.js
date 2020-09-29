80d29af0b3237c167593e1c86d2062f040491611
lewis-munyi <lewis.ndwiga@is.co.ke>
2020-09-30 01:19:02 +0300

    Created Post COntroller

---
+++ api/app/controllers/post.controller.js
@@ -0,0 +1,16 @@
+const Post = require('../models/post.model')
+
+// Create and Save a new Post
+exports.create = (req, res) => {}
+
+// Retrieve and return all Posts from the database.
+exports.findAll = (req, res) => {}
+
+// Find a single Post with a PostId
+exports.findOne = (req, res) => {}
+
+// Update a Post identified by the PostId in the request
+exports.update = (req, res) => {}
+
+// Delete a Post with the specified PostId in the request
+exports.delete = (req, res) => {}
