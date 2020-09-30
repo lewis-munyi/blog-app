59922a431da9618f82ed623bb9b3d1eaf01e7ca3
lewis-munyi <lewis.ndwiga@is.co.ke>
2020-09-30 14:15:10 +0300

    Log in user

--- api/app/controllers/user.controller.js
+++ api/app/controllers/user.controller.js
@@ -1,4 +1,6 @@
 const User = require('../models/user.model')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
 exports.create = (req, res) => {
 	const { firstname, lastname, email, password, photo, social } = req.body
@@ -24,6 +26,49 @@ exports.create = (req, res) => {
 		.catch(error => {
 			res.status(error.status).send({
 				message: error.message,
+			})
+		})
+}
+
+exports.signin = (req, res) => {
+	const { email, password } = req.body
+
+	if (!email || !password) {
+		return res.status(400).send({
+			message: 'Fill out all the fields',
+		})
+	}
+
+	User.findOne({ email })
+		.then(user => {
+			// Successful request
+			if (!user) {
+				// No user
+				return res.status(404).send({
+					message: 'User not found',
+				})
+			}
+
+			if (bcrypt.compareSync(password, user.password)) {
+				// Success - Create web token
+				const token = jwt.sign({ user }, 'yourSecretKey', {
+					expiresIn: '24h',
+				})
+
+				return res.json({
+					user,
+					token,
+				})
+			} else {
+				// Incorrect password
+				res.status(401).json({
+					message: 'Incorrect credentials',
+				})
+			}
+		})
+		.catch(err => {
+			return res.status(500).send({
+				message: 'An internal server error occured: ' + err.message,
 			})
 		})
 }
