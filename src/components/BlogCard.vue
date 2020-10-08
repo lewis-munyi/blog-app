7ebb793f7dfc1ff496ebf90ccf8a3feb3d003cd0
lewis-munyi <lewis.ndwiga@is.co.ke>
2020-10-08 21:38:54 +0300

    Tutorial

---
+++ src/components/BlogCard.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="card border-success mb-3" style="max-width: 20rem;">
+		<div class="card-header">Header</div>
+		<div class="card-body">
+			<h4 class="card-title">Success card title</h4>
+			<p class="card-text">
+				Some quick example text to build on the card title and make up the bulk of the
+				card's content.
+			</p>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'BlogCard',
+	props: {
+		title: String,
+		brief: String,
+		author: String,
+		postID: String,
+		authorID: String,
+		tiemstamp: String,
+	},
+}
+</script>
+
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,11 @@ const routes = [
 		// which is lazy-loaded when the route is visited.
 		component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
 	},
+	{
+		path: '/test',
+		name: 'Test',
+		component: () => import(/* webpackChunkName: "test" */ '../views/Test.vue'),
+	},
 ]
 
 const router = createRouter({
