adeddaec13062a788fdf12ad148180a225452cc9
Gladys-nimo <wkanyora10@gmail.com>
2020-10-09 00:29:43 +0300

    changed the theme

--- src/App.vue
+++ src/App.vue
@@ -19,8 +19,8 @@ export default {
 </script>
 
 <style lang="scss">
-@import url('assets/css/bootstrap.min.css');
-// #app {
+@import url('https://bootswatch.com/4/journal/bootstrap.min.css')
+
 //   font-family: Avenir, Helvetica, Arial, sans-serif;
 //   -webkit-font-smoothing: antialiased;
 //   -moz-osx-font-smoothing: grayscale;
