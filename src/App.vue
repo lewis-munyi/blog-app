26a2ecff7e1a5506f899bdde6b63102b2518e1d0
lewis-munyi <lewis.ndwiga@is.co.ke>
2020-10-09 10:26:20 +0300

    More theme Changes > Added bootstrap, jquery and popper > Added local bootswatch theme

--- src/App.vue
+++ src/App.vue
@@ -19,25 +19,6 @@ export default {
 </script>
 
 <style lang="scss">
-@import url('https://bootswatch.com/4/journal/bootstrap.min.css')
-
-//   font-family: Avenir, Helvetica, Arial, sans-serif;
-//   -webkit-font-smoothing: antialiased;
-//   -moz-osx-font-smoothing: grayscale;
-//   text-align: center;
-//   color: #2c3e50;
-// }
-
-// #nav {
-//   padding: 30px;
-
-//   a {
-//     font-weight: bold;
-//     color: #2c3e50;
-
-//     &.router-link-exact-active {
-//       color: #42b983;
-//     }
-//   }
-// }
+// @import "~bootstrap/scss/bootstrap";
+@import url('assets/css/bootstrap.min.css');
 </style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+window.$ = window.jQuery = require('jquery')
+require('bootstrap/dist/js/bootstrap.bundle')
+
 createApp(App)
 	.use(store)
 	.use(router)
