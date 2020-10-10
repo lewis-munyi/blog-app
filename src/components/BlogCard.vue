926c70ca031a427641fe96e4bad7577b3c561f25
lewis-munyi <lewis.ndwiga@is.co.ke>
2020-10-11 02:53:25 +0300

    Card Styling

--- src/components/BlogCard.vue
+++ src/components/BlogCard.vue
@@ -1,8 +1,8 @@
 <template>
 	<div class="col-sm-12 col-md-6 col-lg-4">
-		<div class="card border-secondary mb-3">
+		<div class="card border-secondary mb-3 blog-card">
 			<img
-				:src="cover || 'https://via.placeholder.com/1366x720?text=' + title.split(' ')[0]"
+				:src="cover || 'https://via.placeholder.com/1366x720/F9D5D4/4C5656?text=' + title.split(' ')[0]"
 				class="card-img-top"
 				style="max-height: 15rem"
 				alt="post cover photo"
@@ -12,18 +12,19 @@
 				<small>
 					By <router-link :to="{ name: 'Blog', params: { authorID } }">{{ author }}</router-link> </small
 				><br />
-				<p class="card-text pt-2">
+				<p class="card-text mt-2">
 					{{ brief + '...' }}
 				</p>
-
+			</div>
+			<div class="card-footer text-muted">
 				<div class="d-flex justify-content-between">
-					<small class="pb-2"> {{ createdAt == updatedAt ? formatDates().created : 'Updated: ' + formatDates().updated }}</small>
+					<span class="my-auto"> {{ createdAt == updatedAt ? formatDates().created : 'Updated: ' + formatDates().updated }}</span>
 					<!-- <a href="#" class="btn btn-ripple btn-primary">Read <i class="ml-1 fas fa-chevron-right"></i></a> -->
 					<div>
 						<!-- Split dropup button -->
 						<div class="btn-group dropup">
 							<a
-								class="btn btn-sm btn-ripple btn-info"
+								class="btn btn-ripple btn-primary"
 								v-bind:class="{ 'rounded-circle btn-sm': !isAuthor }"
 								@click="share(title, brief, postID)"
 								data-toggle="tooltip"
@@ -35,7 +36,7 @@
 							<button
 								v-if="isAuthor"
 								type="button"
-								class="btn btn-sm btn-secondary dropdown-toggle dropdown-toggle-split"
+								class="btn btn-secondary dropdown-toggle dropdown-toggle-split"
 								data-toggle="dropdown"
 								aria-haspopup="true"
 								aria-expanded="false"
@@ -126,8 +127,44 @@
 	}
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 	img {
 		height: 200px;
 	}
+	.blog-card {
+		--bg-color: #fdfdfe;
+		--bg-color-light: #f1f7ff;
+		--text-color-hover: #4c5656;
+		--box-shadow-color: rgba(220, 233, 255, 0.48);
+	}
+
+	.card {
+		box-shadow: 0 14px 26px rgba(0, 0, 0, 0.04);
+		transition: all 0.3s ease-out;
+
+		max-height: 500px;
+		.card-body {
+						overflow: hidden;
+					}
+		&:hover {
+			transform: translateY(-5px) translateX(-5px) scale(1.005) translateZ(0);
+			box-shadow: 0 24px 36px rgba(0, 0, 0, 0.11), 0 24px 46px var(--box-shadow-color);
+			border-color: var(--bg-color-light);
+			background: var(--bg-color);
+		}
+	}
+
+	.card:active {
+		transform: scale(1) translateZ(0);
+		box-shadow: 0 15px 24px rgba(0, 0, 0, 0.11), 0 15px 24px var(--box-shadow-color);
+	}
+
+	.card p {
+		font-size: 17px;
+		color: #4c5656;
+	// 	margin-top: 30px;
+	// 	z-index: 1000;
+	// 	transition: color 0.3s ease-out;
+	}
+
 </style>
